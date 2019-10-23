05823e86488c5062c884ac9ea248aa400b94c098
1041411393@qq.com <1041411393@qq.com>
2019-10-23 21:12:50 +0800

    feat: streamRead

--- src/main/java/com/tju/gowl/io/DictionaryInput.java
+++ src/main/java/com/tju/gowl/io/DictionaryInput.java
@@ -15,6 +15,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.tju.gowl.axiomProcessor.Processor.classAssertion;
 
@@ -31,7 +32,41 @@ public class DictionaryInput {
             BufferedReader bfr = Files.newBufferedReader(fpath);
             String line;
             while ((line = bfr.readLine()) != null) {
-                index = getIndex(index, line);
+                if (!line.contains("\\") && !line.contains("unknown:namespace")) {
+//                    StringTokenizer st = new StringTokenizer(line, " ");
+//                    List<String> list = new ArrayList<>(3);
+//                    while (st.hasMoreElements()) {
+//                        list.add(st.nextToken());
+//                    }
+
+                    List<String> list = Arrays.stream(line.split(" ")).collect(Collectors.toList());
+//            DicRdfDataBean rdfDataBean = new DicRdfDataBean();
+                    String Rs = list.get(0);
+                    String Rp = list.get(1);
+                    String Ro = list.get(2);
+                    int rs = Dictionary.encodeRdf(Rs);
+                    int rp = Dictionary.encodeRdf(Rp);
+                    int ro = Dictionary.encodeRdf(Ro);
+//                  逆角色，等价角色进行替换
+                    if (EquivalentPropertyMap.EquivalentPropertyMap.containsKey(rp)) {
+                        rp = EquivalentPropertyMap.EquivalentPropertyMap.get(rp);
+                    }
+                    if (InversePropertyMap.InverseMap.containsKey(rp)) {
+                        rp = InversePropertyMap.InverseMap.get(rp);
+                        int tmp = rs;
+                        rs = ro;
+                        ro = tmp;
+                    }
+                    DicRdfDataMap.addSourceRdfDataBean(index, rs, rp, ro);
+
+                    index++;
+                    if (index % 100000 == 0) {
+                        System.out.println("finish read " + index + " data");
+                    }
+
+                } else {
+//                    System.out.println(line);
+                }
             }
             bfr.close();
         } catch (IOException e) {
@@ -40,46 +75,6 @@ public class DictionaryInput {
         System.out.println("初始数据数目" + index);
         addClassAssertion(index);
 
-    }
-
-    public static int getIndex(int index, String line) {
-        //TODO 不识别标志
-        if (!line.contains("\\") && !line.contains("unknown:namespace")) {
-            StringTokenizer st = new StringTokenizer(line, " ");
-            List<String> list = new ArrayList<>(3);
-            while (st.hasMoreElements()) {
-                list.add(st.nextToken());
-            }
-
-//                    List<String> list = Arrays.stream(line.split(" ")).collect(Collectors.toList());
-//            DicRdfDataBean rdfDataBean = new DicRdfDataBean();
-            String Rs = list.get(0);
-            String Rp = list.get(1);
-            String Ro = list.get(2);
-            int rs = Dictionary.encodeRdf(Rs);
-            int rp = Dictionary.encodeRdf(Rp);
-            int ro = Dictionary.encodeRdf(Ro);
-//                  逆角色，等价角色进行替换
-            if (EquivalentPropertyMap.EquivalentPropertyMap.containsKey(rp)) {
-                rp = EquivalentPropertyMap.EquivalentPropertyMap.get(rp);
-            }
-            if (InversePropertyMap.InverseMap.containsKey(rp)) {
-                rp = InversePropertyMap.InverseMap.get(rp);
-                int tmp = rs;
-                rs = ro;
-                ro = tmp;
-            }
-            DicRdfDataMap.addSourceRdfDataBean(index, rs, rp, ro);
-
-            index++;
-            if (index % 100000 == 0) {
-                System.out.println("finish read " + index + " data");
-            }
-
-        } else {
-//                    System.out.println(line);
-        }
-        return index;
     }
 
     private static void addClassAssertion(int index) {
